0265c11d7c81642111a10262f32a1e468f93118f
Laurance Bridge <laurancebridge1@gmail.com>
2025-08-26 21:59:57 +0100

    2nd  Draft predictive intelligence_nopriorforecast

---
+++ frontend/src/pages/CohortPerformanceTest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CohortPerformanceDashboard from '@/components/CohortPerformanceDashboard';
+
+const CohortPerformanceTest: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <CohortPerformanceDashboard />
+    </div>
+  );
+};
+
+export default CohortPerformanceTest;
